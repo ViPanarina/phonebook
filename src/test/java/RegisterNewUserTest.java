652f6a1433c02730487e4f191c725a755fbcc691
ViktoriiaPan <you@example.com>
2023-01-24 12:50:33 +0100

    Added assert test

--- src/test/java/RegisterNewUserTest.java
+++ src/test/java/RegisterNewUserTest.java
@@ -2,7 +2,9 @@ import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -20,17 +22,22 @@ public class RegisterNewUserTest extends TestBase {
     By passwordField = By.cssSelector("[placeholder=\"Password\"]");
     By confirmPasswordField = By.name("confirm-password");
     By loginButton = By.xpath("//*[@type=\"submit\"]");
+    By errorMessageBlock = By.id("error-message");
     Faker faker = new Faker();
     @Test
     public void registerNewUser(){
         String userData = faker.internet().emailAddress();
+        String passwordData = faker.internet().password();
+        String expectedErrorMessage = "noErrorMsg";
         driver.findElement(loginForm).isDisplayed();
         driver.findElement(userRegistrationLink).click();
         driver.findElement(registrationForm).isDisplayed();
         fillField(userData, emailField);
-        fillField(userData, passwordField);
-        fillField(userData, confirmPasswordField);
+        fillField(passwordData, passwordField);
+        fillField(passwordData, confirmPasswordField);
         driver.findElement(loginButton).click();
+        String actualMessage = driver.findElement(errorMessageBlock).getText();
+        Assert.assertEquals(actualMessage, expectedErrorMessage);
     }
 
 
