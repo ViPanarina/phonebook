147cffdd671eef65c9086bb21e82594af9e08b1a
ViktoriiaPan <you@example.com>
2023-02-14 23:27:02 +0100

    Added two negative tests in RegisterNewUserTest.java - registerNewUserWithSpaceInTheFirstName , registerNewUserWithShortPassword.

--- src/test/java/e2e/tests/RegisterNewUserTest.java
+++ src/test/java/e2e/tests/RegisterNewUserTest.java
@@ -2,12 +2,13 @@ package e2e.tests;
 
 import com.github.javafaker.Faker;
 import e2e.TestBase;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegisterNewUserTest extends TestBase {
     Faker faker = new Faker();
-    
+
     @Test
     public void registerNewUserWithValidData() {
         //Arrange
@@ -46,13 +47,37 @@ public class RegisterNewUserTest extends TestBase {
         //Arrange
         String userData = "test@gmail.com";
         String passwordData = "test@gmail.com";
-        String expectedErrorMessage = "Error! User already exists e2e.Login now?";
+        String expectedErrorMessage = "Error! User already exists Login now?";
         //Act
         app.getRegister().goToRegistrationPage();
         app.getRegister().fillRegistrationForm(userData, passwordData);
         app.getRegister().clickSignUpButton();
         //Assert
         app.getRegister().checkErrorMessage(app.getRegister().errorMessageBlock, expectedErrorMessage);
+    }
+
+    @Test
+    public void registerNewUserWithSpaceInTheFirstName() {
+        String userData = " ";
+        String passwordData = faker.internet().password();
+        String expectedEmailErrorMessage = "Email is required.";
+
+        app.getRegister().goToRegistrationPage();
+        app.getRegister().fillRegistrationForm(userData, passwordData);
+        Assert.assertTrue(app.getRegister().isElementPresent(By.id("email-error-required")));
+        app.getRegister().checkErrorMessage(By.id("email-error-required"), expectedEmailErrorMessage);
+    }
+
+    @Test
+    public void registerNewUserWithShortPassword() {
+        String userData = "test@gmail.com";
+        String passwordData = "45454";
+        String expectedEmailErrorMessage = "Password must be at least 8 characters.";
+
+        app.getRegister().goToRegistrationPage();
+        app.getRegister().fillRegistrationForm(userData, passwordData);
+        Assert.assertTrue(app.getRegister().isElementPresent(By.id("password-error-minlength")));
+        app.getRegister().checkErrorMessage(By.id("password-error-minlength"), expectedEmailErrorMessage);
     }
 }
 
